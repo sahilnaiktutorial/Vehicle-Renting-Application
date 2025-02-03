16a8d3fd6c127c3af5042b61e7e2d721e95c6aff
sahilnaiktutorial <naiksahil478@gmail.com>
2025-02-03 09:48:53 +0530

    Initial commit

---
+++ src/test/java/com/jsp/vra/VehicleRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.vra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
